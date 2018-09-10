50e2c588834831ab277ebca617e1b652f9b55be1
Joseph <fluxthesky@gmail.com>
2018-09-11 01:26:05 +0800

    Find Contours

--- app/src/main/java/com/example/joseph/fyp/OpenCVTestActivity.java
+++ app/src/main/java/com/example/joseph/fyp/OpenCVTestActivity.java
@@ -10,12 +10,24 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OpenCVTestActivity extends AppCompatActivity {
 
     CameraBridgeViewBase mOpenCvCameraView;
+
+
     private BaseLoaderCallback mLoaderCallback;
+
 
 
     @Override
@@ -23,9 +35,12 @@ public class OpenCVTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_cvtest);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+
+
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener() {
+          mOpenCvCameraView.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener() {
             @Override
             public void onCameraViewStarted(int width, int height) {
 
@@ -38,7 +53,76 @@ public class OpenCVTestActivity extends AppCompatActivity {
 
             @Override
             public Mat onCameraFrame(Mat inputFrame) {
-                return null;
+
+
+                System.gc();
+
+               //Mat returner = new Mat();
+
+                Mat blurredImage = new Mat();
+               // Mat hsvImage = new Mat();
+                Mat mask = new Mat();
+                Mat morphOutput = new Mat();
+
+// remove some noise
+                Imgproc.blur(inputFrame, blurredImage, new Size(7, 7));
+
+// convert the frame to HSV
+
+                Imgproc.cvtColor(blurredImage, mask, Imgproc.COLOR_BGR2HSV);
+
+
+
+                Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2));
+                Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2));
+
+
+
+                Imgproc.erode(mask, morphOutput, erodeElement);
+                Imgproc.erode(mask, morphOutput, erodeElement);
+
+                Imgproc.dilate(mask, morphOutput, dilateElement);
+                Imgproc.dilate(mask, morphOutput, dilateElement);
+
+
+                 Mat hierarchy = new Mat();
+
+                 Imgproc.cvtColor(morphOutput , morphOutput , Imgproc.COLOR_HSV2BGR);
+                Imgproc.cvtColor(morphOutput , morphOutput , Imgproc.COLOR_BGR2GRAY);
+
+                ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+
+
+                Imgproc.findContours(morphOutput, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+
+
+
+                // if any contour exist...
+                if (hierarchy.size().height > 0 && hierarchy.size().width > 0)
+                {
+                    // for each contour, display it in blue
+                    for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0])
+                    {
+
+
+                        Imgproc.drawContours(inputFrame, contours, idx, new Scalar(250, 0, 0));
+                        Log.i("FYP" , "THere was a contour");
+                    }
+                }
+
+              //  inputFrame = morphOutput.clone();
+
+
+                dilateElement.release();
+                erodeElement.release();
+                blurredImage.release();
+                mask.release();
+                morphOutput.release();
+
+
+
+
+                return inputFrame;
             }
         });
 
@@ -68,7 +152,7 @@ public class OpenCVTestActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
     }
 
 }
